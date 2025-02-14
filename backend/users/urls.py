6d15c61c4113f2918c7c9eff09cfa0cf64246760
faseeh1279 <faseehraza1279@gmail.com>
2025-02-14 22:20:08 +0500

    Added new Features

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path 
-from . import views 
+from rest_framework.authtoken.views import obtain_auth_token
+from .views import (UsersListAV, registration_view)
+
 
 urlpatterns = [ 
-    path("list-users/", views.UsersListAV.as_view(), name='list-users'), 
+    path("list-users/", UsersListAV.as_view(), name='list-users'), 
+    path('login/', obtain_auth_token, name='login'), 
+    path("register/", registration_view, name='register'), 
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -2,16 +2,29 @@ from rest_framework.views import APIView
 from rest_framework.response import Response 
 from rest_framework import status 
 from django.contrib.auth.models import User 
-from . import serializers 
+from rest_framework.permissions import IsAuthenticated
+from .serializers import (UserSerializer, RegistrationSerializer) 
 # from rest_framework.serializers import errors 
+from rest_framework.decorators import api_view 
+
+@api_view(['POST'])
+def registration_view(request): 
+    if request.method == "POST": 
+        serializer = RegistrationSerializer(data=request.data)
+        if serializer.is_valid(): 
+            serializer.save() 
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+
 
 class UsersListAV(APIView): 
-    queryset = User.objects.all() 
-    # permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+    permission_classes = [IsAuthenticated]
+
     def get(self, request): 
         user = User.objects.all() 
         if user: 
-            serializer = serializers.UserSerializer(user, many=True, context={'request': request})
+            serializer = UserSerializer(user, many=True, context={'request': request})
             return Response(serializer.data, status=status.HTTP_200_OK)
         else: 
-            return Response(serializer.errors,status=status.HTTP_204_NO_CONTENT)
+            return Response({"error":"Invalid Code..."},status=status.HTTP_204_NO_CONTENT)
